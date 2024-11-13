67fdf1afc06aa22d233890c4b06d80a59b62a409
rbuarquer <buarque.roberta@gmail.com>
2024-11-13 11:36:28 -0300

    Update Teste_3.py

--- Teste_3.py
+++ Teste_3.py
@@ -1,2 +1,4 @@
-st.select_slider("Grau de satisfação",0,100)
-st.write("Grau de satisfação")
+st.title("Avaliação de Satisfação do Cliente")
+st.write("Selecione o grau de satisfação do cliente na escala de 0 a 100.")
+satisfacao = st.select_slider('Grau de Satisfação', options=range(101),  value=50)
+st.write(f"Satisfação do cliente: {satisfacao}/100")
