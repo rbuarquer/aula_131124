c2dc950819b25f5fb51b0e41dc7eb34abe15948f
rbuarquer <buarque.roberta@gmail.com>
2024-11-13 11:43:14 -0300

    Update Teste_3.py

--- Teste_3.py
+++ Teste_3.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Avaliação de Satisfação do Cliente")
 st.write("Selecione o grau de satisfação do cliente na escala de 0 a 100.")
-satisfacao = st.select_slider('Grau de Satisfação', options=range(101),  value=50)
+satisfacao = st.select_slider('Grau de Satisfação', options=range(101))
 st.write(f"Satisfação do cliente: {satisfacao}/100")
