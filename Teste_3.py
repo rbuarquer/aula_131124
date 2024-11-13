a888131df98eadb9c9a5264abc142db9591fc3a2
rbuarquer <buarque.roberta@gmail.com>
2024-11-13 11:38:57 -0300

    Update Teste_3.py

--- Teste_3.py
+++ Teste_3.py
@@ -1,3 +1,4 @@
+import streamlit as st
 st.title("Avaliação de Satisfação do Cliente")
 st.write("Selecione o grau de satisfação do cliente na escala de 0 a 100.")
 satisfacao = st.select_slider('Grau de Satisfação', options=range(101),  value=50)
